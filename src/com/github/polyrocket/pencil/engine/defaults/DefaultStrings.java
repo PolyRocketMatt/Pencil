f2809682a9d29a755289f1c9f5373d45e0fde4af
Matthias <rocketpencil.studios@gmail.com>
2020-07-01 16:19:05 +0200

    Add Language Service

--- src/com/github/polyrocket/pencil/engine/defaults/DefaultStrings.java
+++ src/com/github/polyrocket/pencil/engine/defaults/DefaultStrings.java
@@ -15,6 +15,8 @@ public class DefaultStrings {
     public static final String UNEXPECTED_SERVICE_WITH_ID = "Unexpected service using ID: {0}";
     public static final String NON_EXISTING_SERIVE_WITH_ID = "Service with ID {0} does not exist";
 
+    public static final String LANGUAGE_URL = "https://script.google.com/macros/s/AKfycbz7e4FuRVf-DnW-4JS2kl5jG1HA6DcF0STIpZqLRMqJSdgIaZCa/exec";
+
     public static String format(String str, Object... objects) { return MessageFormat.format(str, objects); }
 
 }
